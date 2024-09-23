1907673e04276d480f42fb44893c1fa0b86ee064
Alexis N <erickjpk1234@gmail.com>
2024-09-23 15:32:44 -0500

    Start

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <>
+            <div className="bg-slate-500">
+                <h1 className= "text-3xl text-blue-950 font-bold">Welcome to Laravel</h1>
+                <p>This is a Laravel React application.</p>
+                <a href="">Ninguna parte</a>
+            </div>
+        </>
+    );
+}
